459360462b7d17091c66a5f1a9429dbe7793aead
Ganiyu idris <golamide27@gmail.com>
2023-09-10 17:54:54 +0100

    Added lenis to layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,38 @@
 <template>
 	<div>
-		<nuxt/>
+		<nuxt />
 	</div>
 </template>
 
 <script>
 export default {
-	
+	data() {
+		return {
+			lenis: null
+		}
+	},
+
+	methods: {
+		initLenis() {
+			this.lenis = new Lenis({ duration: 2 })
+
+			this.lenis.on('scroll', (e) => {
+				// console.log(e)
+			})
+
+			const raf = (time) => {
+				this.lenis.raf(time)
+				requestAnimationFrame(raf)
+			}
+
+			requestAnimationFrame(raf)
+		}
+	},
+
+	mounted() {
+		this.initLenis()
+	}
 }
 </script>
 
-<style lang="postcss">
-	
-</style>
+<style lang="postcss"></style>
