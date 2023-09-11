767b40709ce0e8fed1530db8605043e65999d43f
Ganiyu idris <golamide27@gmail.com>
2023-09-11 13:21:10 +0100

    Done with the about section

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,38 @@
 <template>
   <div class="IndexPage">
+    <section class="Styles">
+    </section>
+    
+    <section class="About">
+      <div class="Container constraint">
+        <h3>
+          About Our Creator
+        </h3>
+
+        <div class="Content md:between">
+          <div class="TopLeft">
+            <p>
+              I'm Carol, based in Toronto, Canada. I've polished my skills in Adobe Creative Suite, with a strong focus on Illustrator and Photoshop.
+              <br/><br/>
+              What makes me stand out is my talent for turning ideas into designs that are seriously captivating. Whether I'm whipping up snazzy brand identities, nailing those pixel-perfect icons, or bringing illustrations to life, I'm all about adding that extra dash of pizzazz.
+              <br/><br/>
+              If you've got a project in mind, don't hesitate to reach out to me!
+            </p>
+
+            <a href="" class="ContactMe">
+              Contact Me
+            </a>
+          </div>
+
+          <div class="BottomRight">
+            <div class="Image">
+              <img src="/images/abb-temp.webp" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    
     <section class="Branding">
       <div class="Container constraint">
         <h4>
@@ -61,6 +94,40 @@ export default {
 
 <style lang="postcss" scoped>
 .IndexPage {
+  .About {
+    @apply my-16 md:my-20 lg:my-24 xl:my-[120px];
+    
+    .Container {
+      @apply space-y-10 md:space-y-14 lg:space-y-16 xl:space-y-[70px] 2xl:space-y-20;
+
+      h3 {
+        @apply text-center
+      }
+
+      .Content {
+        @apply space-y-10 md:space-y-0 md:space-x-5 2xl:space-x-20;
+
+        .TopLeft {
+          @apply md:w-[46%] xl:w-[50%] shrink-0 space-y-5 lg:space-y-7 xl:space-y-10;
+
+          p {
+            @apply xl:-tracking-[0.01em] 2xl:tracking-normal
+          }
+
+          .ContactMe {
+            @apply bg-[#EAEAEA] border-[1.5px] border-[#E5E5E5] rounded-lg xl:rounded-xl py-3 xl:py-4 px-5 xl:px-6 font-semibold text-primary text-sm xl:text-[15px] 2xl:text-base leading-[120%] block w-fit
+          }
+        }
+
+        .BottomRight {
+          .Image {
+            @apply -mr-5 xl:-mr-[100px]
+          }
+        }
+      }
+    }
+  }
+  
   .Branding {
     @apply mt-16 md:mt-20 lg:mt-24 xl:mt-[120px] mb-14 lg:mb-20 xl:mb-[100px]; 
 
