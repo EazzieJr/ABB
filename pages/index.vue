aab5288a5378cc74c4804be0a282e31bb9457cc4
Ganiyu idris <golamide27@gmail.com>
2023-09-11 11:59:44 +0100

    Finished the branding section

--- pages/index.vue
+++ pages/index.vue
@@ -6,18 +6,16 @@
           Branding
         </h4>
 
-        <div class="Content">
-          <div class="Products">
-            <div v-for="(product, index) in products" :key="index" class="Product">
-              <img v-if="product.bg" :src="`/images/products/product-${index+1}-bg-image.webp`" alt="" class="BgImage">
-              <img :src="`/images/products/product-${index+1}.webp`" alt="" class="ActualImage">
-            </div>
+        <div class="Products">
+          <div v-for="(product, index) in products" :key="index" class="Product">
+            <img v-if="product.bg" :src="`/images/products/product-${index+1}-bg-image.webp`" alt="" class="BgImage">
+            <img :src="`/images/products/product-${index+1}.webp`" alt="" class="ActualImage">
           </div>
-
-          <a href="" class="ViewMore">
-            View More
-          </a>
         </div>
+
+        <a href="" class="ViewMore">
+          View More
+        </a>
       </div>
     </section>
   </div>
@@ -67,7 +65,7 @@ export default {
     @apply mt-16 md:mt-20 lg:mt-24 xl:mt-[120px] mb-14 lg:mb-20 xl:mb-[100px]; 
 
     .Container {
-      @apply space-y-10 md:space-y-14 lg:space-y-16 xl:space-y-[70px];
+      @apply space-y-10 md:space-y-14 lg:space-y-16 xl:space-y-[70px] 2xl:space-y-20;
 
       .Products {
         @apply grid grid-cols-1 md:grid-cols-2 gap-5;
@@ -97,6 +95,10 @@ export default {
             }
           }
         }
+      }
+
+      .ViewMore {
+        @apply w-fit mx-auto text-primary bg-secondary rounded-lg xl:rounded-xl px-5 xl:px-[30px] py-3.5 xl:py-4 block font-semibold text-sm xl:text-[15px] !leading-[120%]
       }
     }
   }
