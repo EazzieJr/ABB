51ed6dd47cd7c05c4513153acc1abe4fb50be506
Ganiyu idris <golamide27@gmail.com>
2023-09-11 03:25:15 +0100

    Added new font

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
 	<footer>
 		<div class="Container constraint">
-			<div class="Top">
+			<div class="Top col-center">
 				<div class="Texts">
 					<h5>
 						Let's Work!
@@ -31,7 +31,19 @@ footer {
 	@apply bg-primary;
 
 	.Container {
-		
+		@apply pt-16 md:pt-20 lg:pt-24 xl:pt-[120px];
+
+		.Top {
+			@apply space-y-7 lg:space-y-10 xl:space-y-[50px] text-center;
+
+			.Texts {
+				@apply space-y-4 lg:space-y-6 xl:space-y-[30px];
+
+				h5 {
+					@apply text-white font-semibold text-5xl xl:text-[78px]
+				}
+			}
+		}
 	}
 }
 </style>
