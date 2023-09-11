b13967f86044be23c99afeddcd4467cce007f9c6
Ganiyu idris <golamide27@gmail.com>
2023-09-11 03:18:10 +0100

    Added footer to the layout

---
+++ components/Footer.vue
@@ -0,0 +1,37 @@
+<template>
+	<footer>
+		<div class="Container constraint">
+			<div class="Top">
+				<div class="Texts">
+					<h5>
+						Let's Work!
+					</h5>
+
+					<p>
+						If you've got a project in mind, don't hesitate to reach out to me!
+					</p>
+				</div>
+
+				<button class="ContactMe">
+					Contact Me
+				</button>
+			</div>
+		</div>
+	</footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="postcss" scoped>
+footer {
+	@apply bg-primary;
+
+	.Container {
+		
+	}
+}
+</style>
