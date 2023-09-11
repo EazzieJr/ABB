ca7bfd839417f1efd759822319aa1d237a9275a9
Ganiyu idris <golamide27@gmail.com>
2023-09-11 02:52:29 +0100

    Made inter conastat

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,7 +1,24 @@
 <template>
 	<nav>
-		<div class="NavContainer constraint">
+		<div class="NavContainer constraint between">
+			<nuxt-link to="/">
+				<img src="/svg/logo.svg" alt="">
+			</nuxt-link>
 
+			<div class="Buttons start">
+				<button class="ContactMe">
+					Contact Me
+				</button>
+
+				<button class="DownloadIcon">
+					Download Icon
+				</button>
+				
+				<button class="MenuBurger">
+					<div class="top"></div>
+					<div class="bottom"></div>
+				</button>
+			</div>
 		</div>
 	</nav>
 </template>
@@ -12,12 +29,30 @@ export default {
 }
 </script>
 
-<style lang="postcss">
+<style lang="postcss" scoped>
 nav {
 	@apply bg-primary py-5;
 	
 	.NavContainer {
-		
+		.Buttons {
+			@apply space-x-6;
+
+			.ContactMe, .DownloadIcon {
+				@apply hidden md:block;
+			}
+
+			.ContactMe {
+				@apply text-white
+			}
+			
+			.MenuBurger {
+				@apply space-y-2 md:hidden;
+
+				div {
+					@apply h-0.5 w-8 bg-white;
+				}
+			}
+		}
 	}
 }
 </style>
