1864e2d7bb9e547e68b3b65996f26c550de7e91d
Ganiyu idris <golamide27@gmail.com>
2023-09-11 03:00:17 +0100

    Finished nav

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -31,18 +31,22 @@ export default {
 
 <style lang="postcss" scoped>
 nav {
-	@apply bg-primary py-5;
+	@apply bg-primary py-[21px];
 	
 	.NavContainer {
 		.Buttons {
 			@apply space-x-6;
 
 			.ContactMe, .DownloadIcon {
-				@apply hidden md:block;
+				@apply hidden md:block font-semibold text-sm xl:text-base leading-4 xl:leading-[18px];
 			}
 
 			.ContactMe {
 				@apply text-white
+			}
+
+			.DownloadIcon {
+				@apply text-primary bg-secondary px-4 py-3 xl:px-5 xl:py-[15px] rounded-[10px]
 			}
 			
 			.MenuBurger {
