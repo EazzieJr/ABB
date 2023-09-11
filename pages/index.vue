b8fa577c99eae27911b1e1df2a09b66cfb52969f
Ganiyu idris <golamide27@gmail.com>
2023-09-11 02:36:42 +0100

    Configured fonts

--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,19 @@
 <template>
-  <section class="bg-black h-screen w-4/5">
-  </section>
+  <div class="IndexPage">
+    
+  </div>
 </template>
 
 <script>
-import Lenis from '@studio-freight/lenis'
 
 export default {
   name: 'IndexPage',
 
   methods: {
-    initLenis() {
-      const lenis = new Lenis({duration: 2})
 
-      lenis.on('scroll', (e) => {
-        // console.log(e)
-      })
-
-      function raf(time) {
-        lenis.raf(time)
-        requestAnimationFrame(raf)
-      }
-
-      requestAnimationFrame(raf)
-    }
   }, 
 
   mounted() {
-    this.initLenis()
   }
 }
 </script>
