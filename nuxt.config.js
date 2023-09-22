2b56921b2cccbe634cf0ec4671cf753d4e0ba541
Ganiyu idris <golamide27@gmail.com>
2023-09-22 18:47:21 +0100

    Installed splitting

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,6 +27,7 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     // { src: '@/plugins/vue-flickity.js', mode: 'client' },
+    { src: '~/plugins/splitting.js', mode: 'client' }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
