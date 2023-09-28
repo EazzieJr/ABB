087ea8a112888749e7eac4de8514c59920404046
Ganiyu idris <golamide27@gmail.com>
2023-09-28 18:20:09 +0100

    Cleared console logs

--- pages/index.vue
+++ pages/index.vue
@@ -948,7 +948,7 @@ export default {
       const m2 = gsap.timeline()
       const m3 = gsap.timeline()
 
-      console.log(marqueeOne.clientWidth, marqueeOne.clientWidth/3)
+      // console.log(marqueeOne.clientWidth, marqueeOne.clientWidth/3)
       m1.to(marqueeOne, {
         x: -(marqueeOne.clientWidth/3 + (base.matches ? 10 : lg.matches ? 14 : 20)),
         duration: 25,
